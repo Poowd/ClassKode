9f61ad382b09923aae73038f2aa2ba95fe7a5c1f
Mark Lape <121615238+Poowd@users.noreply.github.com>
2024-08-22 20:06:12 +0800

    Update DefaultButton.js

--- client/src/component/Button/DefaultButton.js
+++ client/src/component/Button/DefaultButton.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-export class DefaultButton extends React.Component {
+export default class DefaultButton extends React.Component {
   render() {
     return (
       <button
